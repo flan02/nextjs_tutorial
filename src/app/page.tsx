350da2dc7086f067e80110dfcbc8fd6e8e05d876
flan02 <chanivetdan@hotmail.com>
2023-07-07 16:33:28 -0300

    Instalamos nextjs inspeccionamos un poco y probamos crear var typescript con codigo tailwindcss para embeber en los componentes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,22 @@
 import Image from 'next/image'
 
+
+//?  ***************** Tailwind CSS
+
+const main = "flex min-h-screen flex-col items-center justify-between p-24";
+const div =
+  "z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex";
+const p =
+  "fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30";
+const footer = "m-0 max-w-[30ch] text-sm opacity-50";
+
+//? ****************** Final CSS
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+    <main className={`${main}`}>
+      <div className={`${div}`}>
+        <p className={`${p}`}>
           Get started by editing&nbsp;
           <code className="font-mono font-bold">src/app/page.tsx</code>
         </p>
@@ -103,11 +115,11 @@ export default function Home() {
               -&gt;
             </span>
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+          <p className={`${footer}`}>
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
         </a>
       </div>
-    </main>
+    </main >
   )
 }
